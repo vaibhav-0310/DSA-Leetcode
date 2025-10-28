001ed6c617478572127f11ed3e5728f18766ffff
Vaibhav bhargav <119117389+vaibhav-0310@users.noreply.github.com>
2025-10-28 20:26:11 +0530

    Time: 19 ms (11.54%) | Memory: 44.5 MB (53.39%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,19 +1,24 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character, Integer> hm1 =new HashMap<>();
-         HashMap<Character, Integer> hm2 =new HashMap<>();
-         for(int i=0;i<s.length();i++){
-            hm1.put(s.charAt(i),hm1.getOrDefault(s.charAt(i),0)+1);
-         }
-         for(int i=0;i<t.length();i++){
-            hm2.put(t.charAt(i),hm2.getOrDefault(t.charAt(i),0)+1);
-         }
-
-         if(hm1.equals(hm2)){
-            return true;
-         }
-         else{
-            return false;
-         }
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        for(int i=0;i<t.length();i++){
+            char c=t.charAt(i);
+            if(map.get(c)!=null){
+                if(map.get(c)==1){
+                    map.remove(c);
+                }
+                else{
+                    map.put(c,map.get(c)-1);
+                }
+            }
+            else{
+                return false;
+            }
+        }
+        return map.isEmpty();
     }
 }
